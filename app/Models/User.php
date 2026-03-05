8727634bc821c7b3e92b9e349d65ff3f706d4c2e
Ahmad Fauzi <fauzi.enginer@gmail.com>
2026-03-05 08:44:49 +0700

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; // ⬅️ pastikan ini ada
 
 class User extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable; // ⬅️ tambahkan HasApiTokens
     use HasFactory, Notifiable;
 
     /**
