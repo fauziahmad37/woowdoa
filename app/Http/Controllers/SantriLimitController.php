750dd24b43fa3e8126f64b8b9bb33f1df2b9533d
hasmi <hasmi@hitcorporation.com>
2026-03-06 11:32:14 +0700

    tambah modul limit santri

---
+++ app/Http/Controllers/SantriLimitController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SantriLimit;
+use Illuminate\Http\Request;
+
+class SantriLimitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SantriLimit $santriLimit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SantriLimit $santriLimit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SantriLimit $santriLimit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SantriLimit $santriLimit)
+    {
+        //
+    }
+}
