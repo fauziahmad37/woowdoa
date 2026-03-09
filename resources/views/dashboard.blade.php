b85ddbac5b97265da1f254dd8e1f8701e065a7ea
putrimanggar10 <putrimanggar08@gmail.com>
2026-03-09 09:25:13 +0700

    update layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,8 +16,8 @@ class="px-4 py-2 rounded-lg text-sm font-semibold">
 Dashboard Transaksi
 </button>
 
-<button @click="tab='santri'"
-:class="tab==='santri' ? 'bg-green-600 text-white' : 'bg-gray-100'"
+<button @click="tab='pesantren'"
+:class="tab==='pesantren' ? 'bg-green-600 text-white' : 'bg-gray-100'"
 class="px-4 py-2 rounded-lg text-sm font-semibold">
 Pesantren
 </button>
@@ -133,8 +133,9 @@ Grafik Transaksi Bulanan
 <!-- end dashboard transaksi -->
 
 <!-- dashboard santri -->
-<div x-show="tab === 'santri'">
+<div x-show="tab === 'pesantren'">
   
+<!-- grid santri -->
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-6">
 
 <!-- Total Santri -->
@@ -218,6 +219,94 @@ Grafik Transaksi Bulanan
 </div>
 
 </div>
+
+<!-- end grid santri -->
+
+<!-- grid ustad -->
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-6">
+
+<!-- Total ustad dan ustazah -->
+<div class="rounded-xl bg-white text-gray-800 p-6 shadow-md w-full">
+<div class="flex items-center justify-between">
+
+<h1 class="text-4xl font-bold">
+{{ number_format($totalTeacher ) }}
+</h1>
+
+<div class="w-10 h-10 flex items-center justify-center rounded-lg bg-green-100 p-2">
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="#029e13" class="w-8 h-8" viewBox="0 0 640 512"><path d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm32 32h-64c-17.6 0-33.5 7.1-45.1 18.6 40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64zm-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zm-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4z"/></svg>
+</div>
+
+</div>
+
+<h1 class="text-md font-bold mt-2">Total Ustadzh & Ustadzah</h1>
+<p class="text-sm text-gray-500 mt-1">Jumlah seluruh santri</p>
+</div>
+
+
+<!-- Ustadzh -->
+<div class="rounded-xl bg-white text-gray-800 p-6 shadow-md w-full">
+<div class="flex items-center justify-between">
+
+<h1 class="text-4xl font-bold">
+{{ number_format($teacherLaki ) }}
+</h1>
+
+<div class="w-10 h-10 flex items-center justify-center rounded-lg bg-green-100 p-2">
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="#029e13" class="w-8 h-8"  viewBox="0 0 192 512"><path d="M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0 24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z"/></svg>
+</div>
+
+</div>
+
+<h1 class="text-md font-bold mt-2">Ustadzh</h1>
+<p class="text-sm text-gray-500 mt-1">Jumlah Ustadzh</p>
+</div>
+
+
+<!-- Ustadzah -->
+<div class="rounded-xl bg-white text-gray-800 p-6 shadow-md w-full">
+<div class="flex items-center justify-between">
+
+<h1 class="text-4xl font-bold">
+{{ number_format($teacherPerempuan ) }}
+</h1>
+
+<div class="w-10 h-10 flex items-center justify-center rounded-lg bg-green-100 p-2">
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="#029e13" class="w-8 h-8"  viewBox="0 0 256 512">
+  <path d="M128 0c35.346 0 64 28.654 64 64s-28.654 64-64 64c-35.346 0-64-28.654-64-64S92.654 0 128 0m119.283 354.179l-48-192A24 24 0 0 0 176 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H80a24 24 0 0 0-23.283 18.179l-48 192C4.935 369.305 16.383 384 32 384h56v104c0 13.255 10.745 24 24 24h32c13.255 0 24-10.745 24-24V384h56c15.591 0 27.071-14.671 23.283-29.821z"/></svg>
+</svg>
+</div>
+
+</div>
+
+<h1 class="text-md font-bold mt-2">Ustadzah</h1>
+<p class="text-sm text-gray-500 mt-1">Jumlah Ustadzah</p>
+</div>
+
+
+<!-- Ustadzh aktif -->
+<div class="rounded-xl bg-white text-gray-800 p-6 shadow-md w-full">
+<div class="flex items-center justify-between">
+
+<h1 class="text-4xl font-bold">
+{{ number_format($teacherAktif ) }}
+</h1>
+
+<div class="w-10 h-10 flex items-center justify-center rounded-lg bg-green-100 p-2">
+<svg xmlns="http://www.w3.org/2000/svg" fill="#029e13" class="w-8 h-8" viewBox="0 0 640 512"><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4zm323-128.4l-27.8-28.1c-4.6-4.7-12.1-4.7-16.8-.1l-104.8 104-45.5-45.8c-4.6-4.7-12.1-4.7-16.8-.1l-28.1 27.9c-4.7 4.6-4.7 12.1-.1 16.8l81.7 82.3c4.6 4.7 12.1 4.7 16.8.1l141.3-140.2c4.6-4.7 4.7-12.2.1-16.8z"/></svg>
+</div>
+
+</div>
+
+<h1 class="text-md font-bold mt-2">Ustadzh & Ustadzah Aktif</h1>
+<p class="text-sm text-gray-500 mt-1">Ustadzh & Ustadzah yang masih aktif</p>
+</div>
+
+</div>
+<!-- end grid ustad -->
 </div>
 
 <!-- end santri -->
