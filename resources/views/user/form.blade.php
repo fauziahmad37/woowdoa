30737dceb5532d8eba07485689a708b033466b77
hasmi <hasmi@hitcorporation.com>
2026-03-20 22:19:06 +0700

    Update Modul User

---
+++ resources/views/user/form.blade.php
@@ -0,0 +1,252 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="py-6">
+  <div class="container mx-auto w-full px-4">
+		<!-- Breadcrumb -->
+		<nav class="text-gray-700 text-sm mb-6" aria-label="Breadcrumb">
+				<ol class="list-reset flex flex-wrap items-center">
+						<li class="flex items-center">
+								<a href="{{ route('user.index') }}" 
+									 class="text-green-600 hover:text-green-800">
+										Data User
+								</a>
+								<span class="mx-2">
+										<svg width="8" height="14" viewBox="0 0 8 14" fill="none">
+												<path fill-rule="evenodd" clip-rule="evenodd"
+														d="M0.646447 0.646447C0.841709 0.451184 1.15829 0.451184 1.35355 0.646447L7.35355 6.64645C7.54882 6.84171 7.54882 7.15829 7.35355 7.35355L1.35355 13.3536C1.15829 13.5488 0.841709 13.5488 0.646447 13.3536C0.451184 13.1583 0.451184 12.8417 0.646447 12.6464L6.29289 7L0.646447 1.35355C0.451184 1.15829 0.451184 0.841709 0.646447 0.646447Z"
+														fill="#1A2130"/>
+										</svg>
+								</span>
+						</li>
+						<li>
+								<span class="text-gray-500">
+										{{ isset($user) ? 'Edit Data User' : 'Tambah User' }}
+								</span>
+						</li>
+				</ol>
+		</nav>
+
+		<!-- Judul -->
+		<h2 class="text-xl font-semibold text-gray-700">
+			Data User
+		</h2>
+		<p class="text-gray-500 mb-6">
+				Silakan isi data   dengan lengkap.
+		</p>
+
+        <!-- user Form -->
+<div class="bg-white shadow-sm sm:rounded-lg p-6">
+   @if ($errors->any())
+    <div class="mb-4 p-4 bg-red-100 text-red-700 rounded">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>• {{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+	 @endif
+
+	<form action="{{ isset($user) ? route('user.update', $user->id) : route('user.store') }}" 
+    method="POST"  enctype="multipart/form-data" >
+            
+                @csrf
+                @if(isset($user))
+                    @method('PUT')
+                @endif
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+ 
+	<div>
+			<label class="block text-gray-700 font-medium mb-1">
+					Nama <span class="text-red-500">*</span>
+			</label>
+			<input type="text" name="complete_name"
+						 value="{{ old('complete_name', $user->complete_name ?? '') }}"
+						 class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500">
+			@error('complete_name')
+					<p class="text-sm text-red-600 mt-1">{{ $message }}</p>
+			@enderror
+	</div>
+	
+	<div>
+			<label class="block text-gray-700 font-medium mb-1">
+					Level <span class="text-red-500">*</span>
+			</label>
+			<select name="user_level_id" 
+					class="w-full border border-gray-300 rounded-lg px-3 py-2 select2">
+
+					<option value="">-- Pilih Level --</option>
+
+					@foreach($user_levels as $level)
+					<option value="{{ $level->user_level_id }}"
+					{{ old('user_level_id', $user->user_level_id ?? '') == $level->user_level_id ? 'selected' : '' }}>
+							{{ $level->user_level_name }}
+					</option>
+					@endforeach
+
+			</select>
+	</div>
+
+	<div>
+			<label class="block text-gray-700 font-medium mb-1">
+					Username <span class="text-red-500">*</span>
+			</label>
+			<input type="text" name="username"
+						 value="{{ old('username', $user->username ?? '') }}"
+						 class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500">
+			@error('username')
+					<p class="text-sm text-red-600 mt-1">{{ $message }}</p>
+			@enderror
+	</div>
+
+
+	<div>
+			<label class="block text-gray-700 font-medium mb-1">
+					Password (Kosongkan jika tidak diganti)<span class="text-red-500">*</span>
+			</label>
+			<input type="text" name="password" value=""
+						 class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500">
+			@error('password')
+					<p class="text-sm text-red-600 mt-1">{{ $message }}</p>
+			@enderror
+	</div>	
+	
+	<div>
+			<label class="block text-gray-700 font-medium mb-1">
+					Phone <span class="text-red-500">*</span>
+			</label>
+			<input type="text" name="phone"
+						 value="{{ old('phone', $user->phone ?? '') }}"
+						 class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500">
+			@error('phone')
+					<p class="text-sm text-red-600 mt-1">{{ $message }}</p>
+			@enderror
+	</div>
+
+
+	<div>
+			<label class="block text-gray-700 font-medium mb-1">
+					Email <span class="text-red-500">*</span>
+			</label>
+			<input type="text" name="email"
+						 value="{{ old('email', $user->email ?? '') }}"
+						 class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500">
+			@error('email')
+					<p class="text-sm text-red-600 mt-1">{{ $message }}</p>
+			@enderror
+	</div>	
+	
+	
+
+	<div>
+	<label class="block text-gray-700 font-medium mb-1">Profil Foto</label>
+		@if(isset($user) && optional($user)->profile_photo)
+		<img src="{{ $user && $user->profile_photo
+		? asset('storage/' . $user->profile_photo)
+		: asset('images/default-avatar.png') }}"
+		class="w-12 h-12 object-cover rounded-full border">
+		@endif
+
+		<input type="file" name="profile_photo" 	class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500">
+	</div>
+										
+	
+ 
+</div>
+
+	<!-- Tombol -->
+	<div class="flex justify-end mt-6"> 
+			<button type="submit"
+					class="text-white px-6 py-2 rounded-lg"
+					style="background: linear-gradient(203.18deg, #01AB14 11.82%, #085410 85.47%);">
+					Simpan Data
+			</button>
+	</div>
+
+            </form>
+        </div>
+
+    </div>
+</div>
+
+<!-- modal success -->
+
+
+@if(session('success'))
+<div id="successModal" class="fixed inset-0 z-50 flex items-center justify-center">
+    <div class="absolute inset-0 bg-gray-500 bg-opacity-40"></div>
+
+    <div id="successModalContent"
+        class="relative bg-white rounded-xl shadow-lg p-6 max-w-md w-11/12 sm:w-full transform scale-95 opacity-0 transition-all duration-200">
+        
+        <div class="flex justify-center mb-4">
+            <div class="w-14 h-14 rounded-full bg-green-100 flex items-center justify-center">
+                <svg class="w-8 h-8 text-green-600" fill="none" stroke="currentColor" stroke-width="3" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M5 13l4 4L19 7" />
+                </svg>
+            </div>
+        </div>
+
+        <h2 class="text-lg font-semibold text-gray-800 text-center">
+            Berhasil!
+        </h2>
+
+        <p class="mt-2 text-sm text-gray-600 text-center">
+            Data limit belanja berhasil ditambahkan.
+        </p>
+
+        <div class="mt-6 flex flex-col sm:flex-row justify-center gap-3">
+
+  
+            <!-- Selesai -->
+            <a href="{{ route('limitbelanja.index') }}"
+                class="px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700">
+                Selesai
+            </a>
+
+        </div>
+    </div>
+</div>
+@endif
+
+
+
+<!-- end modal -->
+   
+<!-- Select2 CSS --> 
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+ 
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/css/select2.min.css" rel="stylesheet"/>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/js/select2.min.js"></script>
+<script> 
+$(document).ready(function(){ 
+	$('#studentSelect').select2({ 
+    width:'100%',
+    dropdownParent: $('body'),   // PENTING
+    placeholder:'Ketik NIS / Nama siswa',
+    minimumInputLength:1,
+    ajax:{
+			url:"{{ route('students.search') }}",
+			dataType:'json',
+			delay:250,
+			data:function(params){
+					return {
+							q:params.term
+					};
+			},
+			processResults:function(data){
+				return {
+						results: data.results
+				};
+			}
+    }
+	});
+ 
+}); 
+</script>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SantriController;
 use App\Http\Controllers\ParentController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\MerchantUserController;
 use App\Http\Controllers\MerchantController;
 use App\Http\Controllers\MerchantCategoryController;
@@ -73,22 +74,16 @@ Route::resource('limitbelanja', LimitBelanjaController::class);
 
 // merchantuser
 Route::prefix('merchant-user')->name('merchant.user.')->group(function(){
+	Route::get('/',[MerchantUserController::class,'index'])->name('index');
+	Route::get('/create',[MerchantUserController::class,'create'])->name('create');
+	Route::post('/store',[MerchantUserController::class,'store'])->name('store');
+	Route::get('/edit/{id}',[MerchantUserController::class,'edit'])->name('edit');
+	Route::put('/update/{id}',[MerchantUserController::class,'update'])->name('update');
+	Route::delete('/delete/{id}',[MerchantUserController::class,'destroy'])->name('destroy');
 
-    Route::get('/',[MerchantUserController::class,'index'])->name('index');
-
-    Route::get('/create',[MerchantUserController::class,'create'])->name('create');
-
-    Route::post('/store',[MerchantUserController::class,'store'])->name('store');
-
-    Route::get('/edit/{id}',[MerchantUserController::class,'edit'])->name('edit');
-
-    Route::put('/update/{id}',[MerchantUserController::class,'update'])->name('update');
-
-    Route::delete('/delete/{id}',[MerchantUserController::class,'destroy'])->name('destroy');
-
-    Route::get('/get-kota/{province_id}', [MerchantUserController::class,'getKota']);
-Route::get('/get-kecamatan/{regency_id}', [MerchantUserController::class,'getKecamatan']);
-Route::get('/get-kelurahan/{district_id}', [MerchantUserController::class,'getKelurahan']);
+	Route::get('/get-kota/{province_id}', [MerchantUserController::class,'getKota']);
+	Route::get('/get-kecamatan/{regency_id}', [MerchantUserController::class,'getKecamatan']);
+	Route::get('/get-kelurahan/{district_id}', [MerchantUserController::class,'getKelurahan']);
 });
 
 
@@ -149,14 +144,18 @@ Route::middleware('auth')->group(function () {
 
 
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // logout
 
- 
+//user
+Route::resource('user', UserController::class);   
+Route::get('/user/userlevel', [UserController::class, 'userlevel'])->name('user.userlevel');
 
+
+
+// logout
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->name('logout');
 
